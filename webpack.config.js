9b912150aedb5f6fbe09025edfe62d8c4e0a84e4
duanwujiedeta <876138964@qq.com>
2022-01-20 17:09:06 +0800

    回滚处理---为管理输出章节做准备

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require('path');
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
 
 module.exports = {
   entry: './src/index.js',
@@ -9,40 +6,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'], //处理顺序，从右到左
-    }, {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    }, {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    }, {
-      test: /\.(csv|tsv)$/i,
-      use: ['csv-loader'],
-    }, {
-      test: /\.xml$/i,
-      use: ["xml-loader"],
-    }, {
-      test: /\.toml$/i,
-      type: "json",
-      parser: {
-        parse: toml.parse,
-      }
-    }, {
-      test: /\.yaml$/i,
-      type: "json",
-      parser: {
-        parse: yaml.parse,
-      }
-    }, {
-      test: /\.json5$/i,
-      type: "json",
-      parser: {
-        parse: json5.parse,
-      }
-    }]
-  }
 };
