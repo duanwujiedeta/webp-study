f2d42792b23b877daac73919e5c86e0a21d02fcb
duanwujiedeta <876138964@qq.com>
2022-01-20 16:10:50 +0800

    加载 .xml .csv 数据

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack','中文'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '中文'], ' ');
   element.classList.add('hello');
 
   // 将图像添加到我们已存在的 div 中。
@@ -14,6 +16,9 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
 
   return element;
