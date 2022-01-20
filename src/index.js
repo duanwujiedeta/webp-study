5f5ae4ea4a3f987c19d82ba04d6a6342d915c0fd
duanwujiedeta <876138964@qq.com>
2022-01-20 17:26:10 +0800

    添加print bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement("button");
 
 
   element.innerHTML = _.join(['Hello', 'webpack', '中文'], ' ');
+
+
+  btn.innerHTML = "Click me an check the console!"
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
 
   return element;
