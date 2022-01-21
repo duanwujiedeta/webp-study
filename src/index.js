18ff6d473eb04d93e59807c370d89b9f3da41cb5
duanwujiedeta <876138964@qq.com>
2022-01-21 16:58:07 +0800

    添加配置，提取不变的vendors，vendors文件名不变

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-async function getComponent() {
-  const element = document.createElement('div');
-  const {default:_} = await import('lodash');
+import _ from "lodash";
+import Print from "./print";
 
-  element.innerHTML = _.join(['Hello', 'webpack', '中文11'], ' ');
+function component() {
+  const element = document.createElement("div");
+
+  // lodash 是由当前 script 脚本 import 进来的
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.onclick = Print.bind(null, "Hello webpack!!!");
+
   return element;
-
 }
 
-import(/* webpackPreload: true */ "./test.js");/* preload会在父级加载的时候并行加载 */
-
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     // publicPath: "/",
   },
   optimization: {
+    moduleIds: "deterministic",
     runtimeChunk: 'single',
     splitChunks: {
       cacheGroups: {
