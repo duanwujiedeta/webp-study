a0b26c69b54ae4c91f45ab39512671b30b2ba292
duanwujiedeta <876138964@qq.com>
2022-01-21 17:55:55 +0800

    生成支持CommonJS/AMD以及script标签使用的library

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'webpack-numbers.js',
-        library: "webpackNumbers", //  生成 window 下面属性webpackNumbers
+        library: {
+            name: 'webpackNumbers',
+            type: 'umd',
+        }
     },
 };
