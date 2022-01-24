c270d2e0bdf2a0362ed2f0d2903197a9773557a9
luyigen <luyigen@empowerwin.com>
2022-01-24 11:57:00 +0800

    添加webpack环境变量，可以在webpack.config.js文件中用函数参数来使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'webpack-numbers.js',
-        library: {
-            name: 'webpackNumbers',
-            type: 'umd',
-        }
-    },
-    externals: {
-        lodash: {
-            commonjs: 'lodash',
-            commonjs2: 'lodash',
-            amd: 'lodash',
-            root: '_',
-        }
-    },
-};
+module.exports = (env) => {
+    console.log('Goal: ', env.goal); // 'local'
+    console.log('Production: ', env.production); // true
+    return {
+        mode: 'development',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'webpack-numbers.js',
+            library: {
+                name: 'webpackNumbers',
+                type: 'umd',
+            }
+        },
+        externals: {
+            lodash: {
+                commonjs: 'lodash',
+                commonjs2: 'lodash',
+                amd: 'lodash',
+                root: '_',
+            }
+        },
+    };
+}
