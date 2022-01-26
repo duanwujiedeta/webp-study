d192d79caa4bc3eb6b3c69eceef82fdc6687f05d
luyigen <luyigen@empowerwin.com>
2022-01-26 13:35:22 +0800

    添加this作为全局变量

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [{
+      test: require.resolve('./src/index.js'),
+      use: 'imports-loader?wrapper=window',
+    }, ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      title: "global this"
+    }),
     new webpack.ProvidePlugin({
       join: ['lodash', 'join'],
     }),
