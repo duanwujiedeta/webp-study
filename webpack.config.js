b49789f26ae219f51373fdb3df0cfc547b710d92
luyigen <luyigen@empowerwin.com>
2022-01-26 12:11:35 +0800

    Shimming，打包只提取lodash的join方法，其他忽略

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      _: 'lodash',
+    }),
+  ],
 };
